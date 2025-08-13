4b327b525af7d9ac78a06eab0f302bfd8003c9d6
Shane <shane.giorgio@gmail.com>
2025-08-13 12:08:36 +0700

    standardized font color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -479,6 +479,10 @@ const SeaHorseWebsite = () => {
               <h3 className="font-bold text-2xl lg:text-2xl">
                 {service.title}
               </h3>
+              
+              {index % 2 === 1 && (
+                <div className="hidden lg:block w-12 h-0.5" />
+              )}
             </div>
 
             {/* Service description */}
@@ -488,11 +492,6 @@ const SeaHorseWebsite = () => {
           </motion.div>
         </motion.div>
       ))}
-    </div>
-
-    {/* Bottom decorative element */}
-    <div className="mt-16 flex justify-center">
-      <div className="w-16 h-0.5 bg-[#760000]"></div>
     </div>
   </div>
 </motion.section>
